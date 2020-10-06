899640ea34fef183cbf42865962e7ed37be0f7d3
MohammadZaheer <zaheerrumjan@gmail.com>
2020-10-06 23:06:46 +0400

    Creation of the file pair.py

---
+++ pair.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def pair(p):
+	for i in p:
+		c = bin(i).strip('0b')
+		if (c.count('1') % 2 == 0):
+			d = '0' + c
+			e = bytearray(d,'utf-8')
+		else:
+			d = '1' + c
+			e = bytearray(d,'utf-8')
+	print(e)
+	
+a = input("Entrez un mot svp: ")
+b = bytearray(a, 'utf-8')
+pair(b)
